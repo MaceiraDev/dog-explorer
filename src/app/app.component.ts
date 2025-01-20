f3dc3e70b27a1bd802239a192f6e461d5e3a4a73
MaceiraDev <leo.maceira.santos@hotmail.com>
2025-01-20 11:48:40 -0300

    Criando o footer, estilizando footer e ajustes em outros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HeaderComponent } from "./components/layout/header/header.component";
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, CommonModule, HeaderComponent],
+  imports: [RouterOutlet, RouterModule, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
